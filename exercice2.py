b006b50d0f4d1c7cb39ce513df42df2b019e9319
alexischrg <alexis.chargy@isen-ouest.yncrea.fr>
2020-03-31 12:02:02 +0200

    Initial commit

---
+++ exercice2.py
@@ -0,0 +1,9 @@
+def reverseString(s, i = 0) :
+   """Reverses the string s recursively """
+   if i == len(s):
+       return s[::-1]
+   return reverseString(s, i+1)
+
+print(reverseString("")) # ""
+print(reverseString("bonjour")) # ruojnob
+print(reverseString("ressasser")) # ressasser
